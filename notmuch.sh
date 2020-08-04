12c057130c266b1fbb8f462c46455d9c88ef682a
Theodor Thornhill <theo@thornhill.no>
2020-08-04 22:53:15 +0200

    Change email setup

--- notmuch.sh
+++ notmuch.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 notmuch new
 notmuch tag -inbox +emacs to:emacs-devel@gnu.org
-notmuch tag -inbox +emacs to:*@debbugs.gnu.org
-notmuch tag -inbox +emacs cc:*@debbugs.gnu.org
+notmuch tag -inbox +emacs-bug to:*@debbugs.gnu.org
+notmuch tag -inbox +emacs-bug cc:*@debbugs.gnu.org
+notmuch tag -inbox +bugs to:~*@todo.sr.ht OR from:~outgoing@sr.ht
 notmuch tag -inbox +sbcl-devel to:sbcl-devel@lists.sourceforge.net
 notmuch tag +allegria from:ensemble.allegria@gmail.com
