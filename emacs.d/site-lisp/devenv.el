7c84e4f92ecb7505073b5730e638618605403c87
Theodor Thornhill <theo@thornhill.no>
2020-09-16 09:04:08 +0200

    Don't use system caret on windows

--- emacs.d/site-lisp/devenv.el
+++ emacs.d/site-lisp/devenv.el
@@ -2,6 +2,7 @@
 
 (when (memq window-system '(w32))
   (setenv "PATH" (concat "C:\\Program Files\\Git\\usr\\bin" ";" (getenv "PATH")))
+  (setq w32-use-visible-system-caret nil)
   (setq ediff-diff-program "C:\\Program Files\\Git\\usr\\bin\\diff.exe")
   (setq magit-git-executable "C:\\Program Files\\Git\\bin\\git.exe"))
 
