ded676ae453d6848fd190c2671ca79d13b788a7b
Theodor Thornhill <theo@thornhill.no>
2021-01-25 19:21:37 +0100

    irc settings

---
+++ emacs.d/site-lisp/ercodor.el
@@ -0,0 +1,14 @@
+;; -*- lexical-binding: t; -*-
+
+
+(defun ercodor-start ()
+  (interactive)
+  (load "~/.emacs.d/erc-auth.el")
+  (erc :server "irc.freenode.net"
+       :port "6667"
+       :nick "theothornhill"
+       :password erc-pass))
+
+(setq erc-autojoin-channels-alist '(("freenode.net" "#lisp" "#emacs" "#sr.ht")))
+
+(provide 'ercodor)
